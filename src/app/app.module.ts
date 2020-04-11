9cc8c1e98738e8e0b4254f2b1fc21cb1b917bb2a
Jakub Sznicer <6390981+cadillaclizard@users.noreply.github.com>
2020-04-11 17:34:30 +0200

    Basic app skeleton
    *  Added @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchedulerComponent,
+    EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
