4e32dd7562c650bd50841b22d4b9a6a6c4b901e8
Jakub Sznicer <6390981+cadillaclizard@users.noreply.github.com>
2020-04-11 18:37:58 +0200

    Added routing & changed layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `<app-layout></app-layout>`
 })
 export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SchedulerComponent } from './scheduler/scheduler.component';
-import { EmployeesComponent } from './employees/employees.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SchedulerComponent,
+    LayoutComponent,
+    NavbarComponent,
     EmployeesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatCardModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
